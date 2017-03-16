debcb11819f34511cc3e42881fc1e360ed928042
Blair Erickson <blair@jamwix.com>
2017-03-16 15:21:21 -0400

    merging new js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -48,11 +48,9 @@ if (annyang) {
   },
     'search *tag': function(tag) {
       console.log("Searching... " + tag);
-      $("#searchbox").val('dave smith'); 
+      $("#searchbox").attr("placeholder", tag);
 
     character = tag;
-    // var PRIV_KEY = "e6abd0558c8f951d1017bdab251fc8c672e6c845";
-    // var PUBLIC_KEY = "31f470f4364dd518ad52e9fe9902ae7e";
     var ts = new Date().getTime();
     var hash = md5(ts + PRIV_KEY + PUBLIC_KEY.toString());
     var marvelAPI = "https://gateway.marvel.com/v1/public/characters?name=" + tag + "&ts=" + ts + "&apikey=" + PUBLIC_KEY + "&hash=" + hash;
